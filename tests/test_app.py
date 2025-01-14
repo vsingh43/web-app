bc7ca9d20bce93d53dab68e37822214ace68f1f7
Vikash Singh <vikash97.singh19@gmail.com>
2025-01-13 20:00:43 -0500

    Adding few more updated code

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,11 @@
+import sys
+import os
+
+# Add the src directory to the Python module search path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+from app import app
+
+
 import pytest
 from src.app import app
 
